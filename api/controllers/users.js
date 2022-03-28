fb6abaad41daee4d912602591cef2cb99765d68d
HevvsOlivia <heather@mcglynn.me.uk>
2022-03-28 20:14:46 +0100

    adding user routes

---
+++ api/controllers/users.js
@@ -0,0 +1,19 @@
+const User = require('../models/user')
+
+router.get('/:email', async function show(req, res) {
+    try {
+        const user = await User.findByEmail(req.params.email)
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).send({err});
+    }
+})
+
+router.get('/', async function index(req, res) {
+    try {
+        const users = await User.all
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(404).send(err);
+    }
+})
