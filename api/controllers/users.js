1a990e62488f5887e4a89d2245b45447c2b80998
HevvsOlivia <heather@mcglynn.me.uk>
2022-03-29 18:46:03 +0100

    minor changes

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middleware/middleware');
 
-router.get('/:email', verifyToken, async function show(req, res) {
+router.get('/:email', /*verifyToken,*/async function show(req, res) {
     try {
         const user = await User.findByEmail(req.params.email)
         res.status(200).json(user);
@@ -13,7 +13,7 @@ router.get('/:email', verifyToken, async function show(req, res) {
     }
 })
 
-router.get('/', verifyToken, async function index(req, res) {
+router.get('/', /*verifyToken,*/ async function index(req, res) {
     try {
         const users = await User.all
         res.status(200).json(users);
@@ -22,9 +22,9 @@ router.get('/', verifyToken, async function index(req, res) {
     }
 })
 
-router.get('/:id', verifyToken, async function users(req, res) {
+router.get('/:id', /*verifyToken,*/ async function users(req, res) {
     try {
-        const users = await User.users
+        const users = await User.findById(req.params.id)
         res.status(200).json(users);
     } catch (err) {
         res.status(404).send(err);
