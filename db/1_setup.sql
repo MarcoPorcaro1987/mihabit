f956fe902855027763696f17dfcb424953e37cde
AlinaLauraV <vizitiu.alinalaura@yahoo.it>
2022-03-29 11:36:43 +0100

    db changes

--- db/1_setup.sql
+++ db/1_setup.sql
@@ -12,15 +12,16 @@ CREATE TABLE habits (
     id serial PRIMARY KEY,
     habit_name varchar(255) NOT NULL,
     habit_description varchar(500) NOT NULL, 
-    habit_frequency int NOT NULL,
-    frequency_target int NOT NULL  
+    habit_frequency varchar(200) NOT NULL,
+    frequency_target int NOT NULL, 
+    email varchar(100) NOT NULL, 
     FOREIGN KEY(email) REFERENCES users(email) 
 );
 
 DROP TABLE IF EXISTS completions;
 CREATE TABLE completions (
     id serial PRIMARY KEY,
-    completion_date varchar(100), 
+    completion_date varchar(200), 
     habit_id int,
     FOREIGN KEY(habit_id) REFERENCES habits(id)
 );
